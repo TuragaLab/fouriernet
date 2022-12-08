a2418f8eaa4c880a7ecdcc4761797e74630d2a92
Diptodip Deb <dd@d2d.sh>
2022-12-08 06:17:19 -0500

    Update layer initializations

--- src/fouriernet/jax/fouriernet.py
+++ src/fouriernet/jax/fouriernet.py
@@ -3,41 +3,43 @@ from jax.core import NamedShape
 from jax.lax import stop_gradient
 import flax.linen as nn
 
-from .layers import FourierConv
+from .layers import FourierConv, SmallFourierConv
 from .utils import he_uniform, fan_in_bias, _compute_fans
 
 from typing import Tuple
-from einops import rearrange
+from einops import rearrange, repeat
 
 
 class FourierNet2D(nn.Module):
 
     fourier_features: int = 5
     fourier_strides: Tuple[int, int] = (2, 2)
+    quantile_scale: float = 0.01
     leaky_relu_slope: float = 0.01
+    precision: str = 'fastest'
 
     @nn.compact
     def __call__(self, image: jnp.ndarray) -> jnp.ndarray:
-        # image, scale = self.input_scaling(image, scale=0.01)
-        image = nn.GroupNorm(num_groups=None, group_size=1, epsilon=1e-5)(image)
+        image, scale = self.input_scaling(image, scale=self.quantile_scale)
+        # image = nn.GroupNorm(num_groups=None, group_size=1, epsilon=1e-5)(image)
 
         image = FourierConv(self.fourier_features, self.fourier_strides)(image)
         image = nn.leaky_relu(image, negative_slope=self.leaky_relu_slope)
         image = nn.GroupNorm(num_groups=None, group_size=1, epsilon=1e-5)(image)
 
         fan_in, _ = _compute_fans(NamedShape(*image.shape[1:-1], self.fourier_features, 1))
-        image = nn.Conv(features=1, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in))(image)
+        image = nn.Conv(features=1, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in), precision=self.precision)(image)
         image = nn.relu(image)
 
-        return image
-        # return image * scale
+        # return image
+        return image * scale
 
     @staticmethod
     def input_scaling(image: jnp.ndarray, scale: float) -> Tuple[jnp.ndarray, float]:
         """Scales image, returns image / (median(image) * scale).
         ** Stops gradients on scale**."""
-        # We dont need the gradient as we rescale later, and since its a big number itll be more stable
-        normed_scale = stop_gradient(jnp.median(image) * scale)
+        # normed_scale = stop_gradient(jnp.median(image) * scale)
+        normed_scale = jnp.median(image) * scale
         return image / normed_scale, normed_scale
 
 
@@ -50,39 +52,59 @@ VmapFourierNet2D = nn.vmap(
     split_rngs={"params": True},
 )
 
+
 class FourierNet3D(nn.Module):
-    n_planes: int = 12
-    n_features_per_plane: int = 5
+    num_planes: int = 32
+    features_per_plane: int = 5
     fourier_strides: Tuple[int, int] = (2, 2)
+    quantile_scale: float = 0.01
     leaky_relu_slope: float = 0.01
+    precision: str = 'fastest'
 
     @nn.compact
     def __call__(self, image: jnp.ndarray) -> jnp.ndarray:
-        image, scale = self.input_scaling(image, scale=0.01)
+        image, scale = self.input_scaling(image, scale=self.quantile_scale)
 
         image = FourierConv(
-            self.n_features_per_plane * self.n_planes, self.fourier_strides,
+            self.features_per_plane * self.num_planes, self.fourier_strides,
         )(image)
         image = nn.leaky_relu(image, negative_slope=self.leaky_relu_slope)
         image = nn.GroupNorm(num_groups=None, group_size=1, epsilon=1e-5)(image)
 
-        image = rearrange(image, "b h w (c d) -> b d h w c", d=self.n_planes)
+        image = rearrange(image, "b h w (c d) -> b d h w c", d=self.num_planes)
 
-        image = nn.Conv(features=5, kernel_size=(11, 7, 7), strides=(1, 1, 1))(image)
+        image = SmallFourierConv(self.features_per_plane, (11, 7, 7))(image)
         image = nn.leaky_relu(image, negative_slope=self.leaky_relu_slope)
         image = nn.GroupNorm(num_groups=None, group_size=1, epsilon=1e-5)(image)
 
-        image = nn.Conv(features=1, kernel_size=(11, 7, 7), strides=(1, 1, 1))(image)
+        image = SmallFourierConv(1, (11, 7, 7))(image)
         image = nn.relu(image)
+
         return image * scale
 
     @staticmethod
     def input_scaling(image: jnp.ndarray, scale: float) -> Tuple[jnp.ndarray, float]:
-        """Scales image, returns image / (median(image) * scale).
-        ** Stops gradients on scale**."""
-        # We dont need the gradient as we rescale later, and since its a big number itll be more stable
-        normed_scale = stop_gradient(jnp.median(image) * scale)
+        """
+        Scales image, returns image / (median(image) * scale).
+        ** Stops gradients on scale**.
+        """
+        # normed_scale = stop_gradient(jnp.median(image) * scale)
+        normed_scale = jnp.median(image) * scale
         return image / normed_scale, normed_scale
+
+
+class ManuallyChunkedFourierNet3D(nn.Module):
+    num_chunks: int = 8
+    num_planes: int = 32
+    features_per_plane: int = 5
+    fourier_strides: Tuple[int, int] = (2, 2)
+    quantile_scale: float = 0.01
+    leaky_relu_slope: float = 0.01
+    precision: str = 'fastest'
+
+    @nn.compact
+    def __call__(self, image: jnp.ndarray) -> jnp.ndarray:
+        return jnp.concatenate([FourierNet3D(num_planes=self.num_planes)(image) for i in range(self.num_chunks)], axis=1)
 
 
 class FourierNetRGB(nn.Module):
@@ -90,25 +112,27 @@ class FourierNetRGB(nn.Module):
     fourier_features: int = 20
     fourier_strides: Tuple[int, int] = (2, 2)
     leaky_relu_slope: float = 0.01
+    use_running_average: bool = False
+    precision: str = 'fastest'
 
     @nn.compact
     def __call__(self, image: jnp.ndarray) -> jnp.ndarray:
         image = FourierConv(self.fourier_features, self.fourier_strides)(image)
         image = nn.leaky_relu(image, negative_slope=self.leaky_relu_slope)
-        image = nn.BatchNorm(use_running_average=False)(image)
+        image = nn.BatchNorm(use_running_average=self.use_running_average)(image)
 
         fan_in, _ = _compute_fans(NamedShape(*image.shape[1:-1], self.fourier_features, 1))
-        image = nn.Conv(features=64, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in))(image)
+        image = nn.Conv(features=64, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in), precision=self.precision)(image)
         image = nn.leaky_relu(image, negative_slope=self.leaky_relu_slope)
-        image = nn.BatchNorm(use_running_average=False)(image)
+        image = nn.BatchNorm(use_running_average=self.use_running_average)(image)
 
         fan_in, _ = _compute_fans(NamedShape(11, 11, 64, 64))
-        image = nn.Conv(features=64, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in))(image)
+        image = nn.Conv(features=64, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in), precision=self.precision)(image)
         image = nn.leaky_relu(image, negative_slope=self.leaky_relu_slope)
-        image = nn.BatchNorm(use_running_average=False)(image)
-        
+        image = nn.BatchNorm(use_running_average=self.use_running_average)(image)
+
         fan_in, _ = _compute_fans(NamedShape(11, 11, 64, 3))
-        image = nn.Conv(features=3, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in))(image)
+        image = nn.Conv(features=3, kernel_size=(11, 11), strides=(1, 1), padding='SAME', kernel_init=he_uniform(), bias_init=fan_in_bias(fan_in), precision=self.precision)(image)
         image = nn.relu(image)
 
         return image
--- src/fouriernet/jax/layers.py
+++ src/fouriernet/jax/layers.py
@@ -5,14 +5,14 @@ from jax import vmap
 from jax.core import NamedShape
 from jax.random import PRNGKey
 
-
-from .utils import complex_he_uniform, _compute_fans, fan_in_bias, next_order
-
 from typing import Tuple, Callable, Optional, Union, Sequence, Any
-Dtype = Any
-Shape = Any
 
 from functools import partial
+
+from .utils import next_order, _compute_fans, he_uniform, complex_he_uniform, fan_in_bias
+
+DType = Any
+
 
 class FourierConv(nn.Module):
     """
@@ -37,7 +37,7 @@ class FourierConv(nn.Module):
     strides: Optional[Tuple[int, ...]] = None
     padding: Union[str, Sequence[Tuple[int, ...]]] = "SAME"
     use_bias: bool = True
-    dtype: Dtype = jnp.float32
+    dtype: DType = jnp.float32
     kernel_init: Callable = complex_he_uniform
     bias_init: Callable = fan_in_bias
 
@@ -93,7 +93,7 @@ class FourierConv(nn.Module):
         fft = partial(jnp.fft.fftn, axes=fft_axes)
         ifft = partial(jnp.fft.ifftn, axes=fft_axes)
 
-        # transform signals and perform Fourier convolution
+        # transform signal, padding to correct shape
         y = fft(jnp.pad(inputs, tuple((0, ffts - ins) for ins, ffts
                                       in zip(inputs.shape, (inputs.shape[0],) + fft_shape + (inputs.shape[-1],)))))
         if strides != ((1,) * ndim):
@@ -105,6 +105,7 @@ class FourierConv(nn.Module):
                 limit_indices=tuple(s for s in y.shape),
                 strides=(1,) + strides + (1,),
             )
+        # perform Fourier convolution
         y = ifft(y[..., jnp.newaxis] * complex_kernel)
 
         # get real features
@@ -145,5 +146,143 @@ class FourierConv(nn.Module):
             fan_in, _ = _compute_fans(NamedShape(*kernel_shape))
             bias = self.param("bias", self.bias_init(fan_in, dtype=self.dtype), NamedShape(self.features,))
             bias.reshape((1,) * (y.ndim - bias.ndim) + bias.shape)
-            y += bias
+            y = y + bias
+        return y
+
+
+class SmallFourierConv(nn.Module):
+    """
+    Convolution Module implemented using the Fourier convolution
+    theorem (for n dimensional input).  Weights are not parameterized
+    in Fourier space, so their shape does not need to match the shape
+    of the input. Instead, the kernel is Fourier transformed for each
+    forward pass and padded to the shape of the input. This is
+    intended for performing convolutions that are too large to be
+    efficient in direct convolution, but that do not need to have a
+    fully global view of the input. Further, this means that the
+    kernel parameters themselves will not take up too much memory.
+
+    Args:
+      features: number of convolution filters.
+      kernel_size: shape of the convolutional kernel, specified as a
+        sequence of integers.
+      strides: a sequence of `n` integers, representing the inter-window
+        strides.
+      padding: either the string `'SAME'`, the string `'VALID'`, or a sequence
+        of `n` `(low, high)` integer pairs that give the padding to apply before
+        and after each spatial dimension.
+      use_bias: whether to add a bias to the output (default: True).
+      dtype: the dtype of the computation (default: float32).
+      kernel_init: initializer for the convolutional kernel.
+      bias_init: initializer for the bias.
+    """
+
+    features: int
+    kernel_size: Tuple[int, ...]
+    strides: Optional[Tuple[int, ...]] = None
+    padding: Union[str, Sequence[Tuple[int, ...]]] = "SAME"
+    use_bias: bool = True
+    dtype: DType = jnp.float32
+    kernel_init: Callable = he_uniform
+    bias_init: Callable = fan_in_bias
+
+    @nn.compact
+    def __call__(self, inputs: jnp.ndarray) -> jnp.ndarray:
+        """
+        Applies a convolution to the inputs using the Fourier convolution
+        theorem.
+
+        Args:
+          inputs: input data with dimensions (batch, spatial_dims..., features).
+        Returns:
+          The convolved data.
+        """
+        # extract shapes used to make kernel from input (only matters on first
+        # call)
+        in_features = inputs.shape[-1]
+        # spatial shape of kernel is already specified
+        kernel_size = self.kernel_size
+        ndim = len(kernel_size)
+        if self.strides is None:
+            strides = (1,) * ndim
+        else:
+            strides = self.strides
+
+        kernel_shape = kernel_size + (
+            in_features,
+            self.features,
+        )
+
+        kernel = self.param("kernel", self.kernel_init(dtype=self.dtype), kernel_shape)
+
+        # get padded shape to prevent circular convolution
+        # TODO(dd): reimplement computing fast shape for FFTs
+        signal_fft_shape = tuple(k1 + k2 for k1, k2 in zip(inputs.shape[1:ndim + 1], kernel_size))
+        signal_fft_shape = tuple(s + 1 if (s % 2 == 1) else s for s in signal_fft_shape)
+        signal_fft_axes = tuple(range(1, ndim + 1))
+        signal_fft = partial(jnp.fft.fftn, axes=signal_fft_axes)
+        ifft = partial(jnp.fft.ifftn, axes=signal_fft_axes)
+
+        # transform signal, padding to correct shape
+        y = signal_fft(jnp.pad(inputs, tuple((0, ffts - ins) for ins, ffts
+                                      in zip(inputs.shape, (inputs.shape[0],) + signal_fft_shape + (inputs.shape[-1],)))))
+        if strides != ((1,) * ndim):
+            # apply stride by subsampling both the transformed input
+            # and the transformed kernel
+            total_dims = len(y.shape)
+            y = lax.slice(
+                y,
+                start_indices=tuple(0 for s in y.shape),
+                limit_indices=tuple(s for s in y.shape),
+                strides=(1,) + strides + (1,),
+            )
+
+        # transform kernel, padding to correct shape 
+        kernel_fft_axes = tuple(range(0, ndim))
+        kernel_fft_shape = tuple(k2 for k2 in y.shape[1:ndim + 1])
+        kernel_fft = partial(jnp.fft.fftn, s=kernel_fft_shape, axes=kernel_fft_axes)
+        complex_kernel = kernel_fft(kernel)
+
+        # perform fourier convolution
+        y = ifft(y[..., jnp.newaxis] * complex_kernel)
+
+        # get real features
+        y = y.real
+
+        if self.padding == "SAME":
+            # crop image back to size of input
+            start_idx = (
+                (0,)
+                + tuple(
+                    (k2 - k1) // 2
+                    for k1, k2 in zip(
+                        inputs.shape[1 : ndim + 1],
+                        y.shape[1 : ndim + 1],
+                    )
+                )
+                + (0, 0)
+            )
+            stop_idx = (
+                (inputs.shape[0],)
+                + tuple(
+                    si + k1
+                    for si, k1 in zip(
+                        start_idx[1 : ndim + 1],
+                        inputs.shape[1 : ndim + 1],
+                    )
+                )
+                + (inputs.shape[-1], self.features)
+            )
+            y = lax.slice(y, start_idx, stop_idx)
+        else:
+            return NotImplemented
+
+        # sum over input feature dimension
+        y = jnp.sum(y, axis=-2)
+
+        if self.use_bias:
+            fan_in, _ = _compute_fans(NamedShape(*kernel_shape))
+            bias = self.param("bias", self.bias_init(fan_in, dtype=self.dtype), NamedShape(self.features,))
+            bias.reshape((1,) * (y.ndim - bias.ndim) + bias.shape)
+            y = y + bias
         return y
